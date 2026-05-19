383807da1ac0f4646b3ef66a0a05b263d89e087a
NOIR-Z314 <am.noirz7247doragon@gmail.com>
2026-05-19 14:15:27 +0500

    Implement shopping cart functionality in script.js

---
+++ script.js
@@ -0,0 +1,63 @@
+let cart = [];
+let total = 0;
+
+function addToCart(item, price) {
+
+  cart.push({ item, price });
+
+  total += price;
+
+  updateCart();
+
+}
+
+function updateCart() {
+
+  const cartItems = document.getElementById("cart-items");
+
+  cartItems.innerHTML = "";
+
+  cart.forEach((food) => {
+
+    let li = document.createElement("li");
+
+    li.innerHTML = `
+      ${food.item}
+      <span>$${food.price}</span>
+    `;
+
+    cartItems.appendChild(li);
+
+  });
+
+  document.getElementById("total").innerText = total;
+
+}
+
+function checkout() {
+
+  if(cart.length === 0) {
+
+    alert("Your cart is empty!");
+
+  } else {
+
+    alert(
+      "Order placed successfully!\nTotal Bill: $" + total
+    );
+
+    cart = [];
+    total = 0;
+
+    updateCart();
+
+  }
+
+}
+
+function scrollToMenu() {
+
+  document.getElementById("menu")
+    .scrollIntoView({ behavior: "smooth" });
+
+}
